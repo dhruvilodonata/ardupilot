05fd2d5e9c6468b5af341fc2d41464a774a1a238
dhruvilodonata <139759134+dhruvilodonata@users.noreply.github.com>
2023-07-17 12:15:06 +0200

    Update AP_MotorsMatrix.cpp
    The quadcopter with X configuration with angles are 42.6, 132.6, -132.6, -42.6 degrees.

--- libraries/AP_Motors/AP_MotorsMatrix.cpp
+++ libraries/AP_Motors/AP_MotorsMatrix.cpp
@@ -597,6 +597,18 @@ bool AP_MotorsMatrix::setup_quad_matrix(motor_frame_type frame_type)
         add_motors(motors, ARRAY_SIZE(motors));
         break;
     }
+    case MOTOR_FRAME_TYPE_odo: {
+        _frame_type_string = "odo";
+        // symmetric frame but angle is 42.6 degree
+        static const AP_MotorsMatrix::MotorDef motors[] {
+            {   42.6, AP_MOTORS_MATRIX_YAW_FACTOR_CW,   1 },
+            { -132.6, AP_MOTORS_MATRIX_YAW_FACTOR_CW,   3 },
+            {  -42.6, AP_MOTORS_MATRIX_YAW_FACTOR_CCW,  4 },
+            {  132.6, AP_MOTORS_MATRIX_YAW_FACTOR_CCW,  2 },
+        };
+        add_motors(motors, ARRAY_SIZE(motors));
+        break;
+    }
 #if APM_BUILD_TYPE(APM_BUILD_ArduPlane)
     case MOTOR_FRAME_TYPE_NYT_PLUS: {
         _frame_type_string = "NYT_PLUS";
